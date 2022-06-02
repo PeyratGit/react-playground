0f4880fe2eadb8017b2a3bf937cb4aef2e4ab507
clement peyrat <aiglefeu31@gmail.com>
2022-06-02 23:04:37 +0200

    Weather app done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -214,6 +214,15 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 //   }
 // }
 // root.render(<Counter/>)
+
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  rootElement
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
