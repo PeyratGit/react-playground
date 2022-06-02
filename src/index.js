97df0c4aeec5068bf7a4b68339845cc2b7b6e379
clement peyrat <aiglefeu31@gmail.com>
2022-06-02 22:08:14 +0200

    Commented counter

--- src/index.js
+++ src/index.js
@@ -188,6 +188,33 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 // }
 // root.render(<Clock />)
 
+// Simple Counter
+// class Counter extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       count : 0
+//     }
+//   }
+
+//   increment = () => {
+//     this.setState({count: this.state.count + 1})
+//   }
+//   decrement = () => {
+//     this.setState({ count: this.state.count - 1 })
+//   }
+//   render(){
+//     return (
+//       <div className="App">
+//         <button onClick={this.increment}>+</button>
+//         <button onClick={this.decrement}>-</button>
+//         <h2>{this.state.count}</h2>
+//       </div>
+//     );
+//   }
+// }
+// root.render(<Counter/>)
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
